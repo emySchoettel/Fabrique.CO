88da7bc290d640822f52ed3d277c9f197c125f60
emySchoettel <e.schoettel@ludus-academie.com>
2020-11-05 12:34:39 +0100

    Ajout singleton

---
+++ Assets/Scripts/HealthScriptSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScriptSingleton : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ManagerSingleton.instance.HP--; 
+        }
+        else if(ManagerSingleton.instance.HP == 0)
+        {
+            Destroy(gameObject);
+        }
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            ManagerSingleton.instance.AddHealth(); 
+        }
+    }
+
+}
---
+++ Assets/Scripts/ManagerSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerSingleton : MonoBehaviour
+{
+
+    public static ManagerSingleton instance = null; 
+    public int HP = 3; // Hit point, variable utilisable sur un autre objet
+
+    private void Awake() {
+        if(instance == null)
+        {
+            instance = this; 
+        }    
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void AddHealth()
+    {
+        HP++;
+    }
+}
